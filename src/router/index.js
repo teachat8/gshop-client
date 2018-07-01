d84404449484a432fabe215d3960d7dcfdd7d770
teachat <teachat@aliyun.com>
2018-07-01 16:45:15 +0800

    注册、登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import MSite from '../pages/MSite/MSite.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
-
+import Login from '../pages/Login/Login.vue'
 
 //声明使用插件
 Vue.use(VueRouter)
@@ -17,23 +17,39 @@ export default new VueRouter({
   routes : [
     {
       path : '/msite',
-      component :MSite
+      component :MSite,
+      meta : {
+        showFooter : true
+      }
     },
     {
       path : '/order',
-      component : Order
+      component : Order,
+      meta : {
+        showFooter : true
+      }
     },
     {
       path : '/profile',
-      component : Profile
+      component : Profile,
+      meta : {
+        showFooter : true
+      }
     },
     {
       path : '/search',
-      component : Search
+      component : Search,
+      meta : {
+        showFooter : true
+      }
     },
     {
       path : '/',
       redirect : '/msite'
+    },
+    {
+      path : '/login',
+      component : Login
     }
   ]
 })
