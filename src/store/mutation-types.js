429f0dec09291e38443056f6dfc40caffeaabca4
teachat <teachat@aliyun.com>
2018-07-15 17:46:19 +0800

    食物分类列表的滑动

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -7,3 +7,7 @@ export const RECEIVE_SHOPS = 'receive_shops'                    //  接收商家
 export const RECEIVE_USER_INFO = 'receive_user_info'                //   接收用户信息
 export const RESET_USER_INFO = 'reset_user_info'                               //   重置用户信息
 
+export const RECEIVE_GOODS = 'receive_goods'                           //   接收商品数组
+export const RECEIVE_RATINGS = 'receive_ratings'                                 //   接收商家评价数组
+export const RECEIVE_INFO = 'receive_info'             //   接收商家信息
+
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,16 @@
 /**
  *  直接更新state的多个方法的对象
  */
-import {RECEIVE_ADDRESS, RECEIVE_CATEGORYS, RECEIVE_SHOPS, RECEIVE_USER_INFO, RESET_USER_INFO} from './mutation-types'
+import {
+  RECEIVE_ADDRESS,
+  RECEIVE_CATEGORYS,
+  RECEIVE_SHOPS,
+  RECEIVE_USER_INFO,
+  RESET_USER_INFO,
+  RECEIVE_GOODS,
+  RECEIVE_RATINGS,
+  RECEIVE_INFO
+} from './mutation-types'
 export default {
   [RECEIVE_ADDRESS] (state, {address}) {
     state.address = address
@@ -17,6 +26,16 @@ export default {
   },
   [RESET_USER_INFO] (state) {
     state.userInfo = {}
-  }
+  },
+
+  [RECEIVE_GOODS] (state, {goods}) {
+    state.goods = goods
+  },
+  [RECEIVE_RATINGS] (state, {ratings}) {
+    state.ratings = ratings
+  },
+  [RECEIVE_INFO] (state, {info}) {
+    state.info = info
+  },
 
 }
